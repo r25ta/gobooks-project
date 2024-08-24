953bb855fee2ce2c53d2aae82b7fae65456bdbb4
r25ta <twentyfive@hotmail.com>
2024-08-24 12:03:26 -0300

    First commit

--- internal/web/handler.go
+++ internal/web/handler.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"gobooks/internal/service"
 	"net/http"
+	"strconv"
 )
 
 type BookHandlers struct {
@@ -49,4 +50,75 @@ func (h *BookHandlers) CreateBook(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(book)
 
+}
+
+func (h *BookHandlers) GetBookByID(w http.ResponseWriter, r *http.Request) {
+	idStr := r.PathValue("id")
+	id, err := strconv.Atoi(idStr)
+
+	if err != nil {
+		http.Error(w, "invalid book ID", http.StatusBadRequest)
+		return
+	}
+
+	book, err := h.service.GetBookByID(id)
+
+	if err != nil {
+		http.Error(w, "failed to get book", http.StatusInternalServerError)
+		return
+	}
+
+	if book == nil {
+		http.Error(w, "book not found", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(book)
+
+}
+
+func (h *BookHandlers) DeleteBook(w http.ResponseWriter, r *http.Request) {
+	idStr := r.PathValue("id")
+	id, err := strconv.Atoi(idStr)
+
+	if err != nil {
+		http.Error(w, "invalid book id", http.StatusBadRequest)
+		return
+	}
+
+	if err := h.service.DeleteBook(id); err != nil {
+		http.Error(w, "failed to delete book", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+
+}
+
+func (h *BookHandlers) UpdateBook(w http.ResponseWriter, r *http.Request) {
+	idStr := r.PathValue("id")
+	id, err := strconv.Atoi(idStr)
+
+	if err != nil {
+		http.Error(w, "invalid book id", http.StatusBadRequest)
+		return
+	}
+
+	var book service.Book
+
+	if err := json.NewDecoder(r.Body).Decode(&book); err != nil {
+		http.Error(w, "invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	book.ID = id
+
+	if err := h.service.UpdateBook(book); err != nil {
+		http.Error(w, "failed to update boook", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(book)
 }
